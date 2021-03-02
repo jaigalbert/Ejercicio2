372b30744fb249699f1159779628ead333869a4b
Alberto Iribe Gonzalez <jaigalbert@gmail.com>
2021-03-01 19:32:48 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Divisible.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Ejercicio2
+{
+    partial class Divisible
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Divisible));
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnDividir.Location = new System.Drawing.Point(245, 36);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 36);
+            this.btnDividir.TabIndex = 4;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblNumero1.Location = new System.Drawing.Point(26, 21);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(70, 17);
+            this.lblNumero1.TabIndex = 0;
+            this.lblNumero1.Text = "Numero 1";
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNumero2.Location = new System.Drawing.Point(113, 65);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero2.TabIndex = 3;
+            this.txtNumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNumero1.Location = new System.Drawing.Point(113, 21);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero1.TabIndex = 1;
+            this.txtNumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblNumero2.Location = new System.Drawing.Point(26, 65);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(70, 17);
+            this.lblNumero2.TabIndex = 2;
+            this.lblNumero2.Text = "Numero 2";
+            // 
+            // Divisible
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 132);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.btnDividir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Divisible";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Divisible DP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+    }
+}
+
---
+++ Ejercicio2/Divisible.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Divisible : Form
+    {
+        public Divisible()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int Resultado = 0, Residuo = 0, numero1=0,numero2=0;
+            if (btnDividir.Text.Equals("Dividir"))
+            {
+                
+                if ((esEntero(txtNumero1.Text)) && ((esEntero(txtNumero2.Text))))
+                {
+                    numero1 = Int32.Parse(txtNumero1.Text);
+                    numero2 = Int32.Parse(txtNumero2.Text);
+                    Resultado = numero1 / numero2;
+                    Residuo = numero1 % numero2;
+                    lblNumero1.Text = "Resultado";
+                    txtNumero1.Text = Resultado.ToString();
+                    txtNumero1.Enabled = false;
+                    lblNumero2.Text = "Residuo";
+                    txtNumero2.Text = Residuo.ToString();
+                    txtNumero2.Enabled = false;
+                    if(Residuo==0)
+                        MessageBox.Show("El número " + numero1.ToString() + " si es divisible entre el número " + numero2.ToString() + ".") ;
+                    else
+                        MessageBox.Show("El número " + numero1.ToString() + " No es divisible entre el número " + numero2.ToString() + ".");
+                    btnDividir.Text="Nuevo";
+                }
+                else { MessageBox.Show("Los numeros no son enteros"); }
+            }
+            else {
+                lblNumero1.Text = "Numero 1";
+                txtNumero1.Enabled = true;
+                lblNumero2.Text = "Numero 2";
+                txtNumero2.Enabled = true;
+                btnDividir.Text = "Dividir";
+
+            }
+        }
+
+        private bool esEntero(string numero)
+        {
+            try
+            {
+                Int32.Parse(numero);
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
